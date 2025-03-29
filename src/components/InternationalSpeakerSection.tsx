8a40edd07421c6bafc70a780289ca8c19845fae8
sumaiyakhn1 <sumaiyakn28@gmail.com>
2025-03-30 03:40:41 +0530

    updates

--- src/components/InternationalSpeakerSection.tsx
+++ src/components/InternationalSpeakerSection.tsx
@@ -56,9 +56,11 @@ const InternationalSpeakerSection = () => {
             </div>
             
             <div className="pt-4">
-              <Button className="bg-gradient-to-r from-teal-500 to-teal-300 hover:from-red-700 hover:to-red-600 text-white shadow-md font-medium px-6">
-                View Profile
-              </Button>
+            <a href="/https://www.aptitudehealth.com/oncology-news/expert-perspectives-breast-cancer-marc-thill/" className="inline-block mt-6">
+          <button className="bg-gradient-to-r from-teal-500 to-teal-300 hover:from-red-700 hover:to-red-600 text-white shadow-md font-medium px-6 py-2 rounded-lg">
+            View Profile
+          </button>
+        </a>
             </div>
           </div>
         </div>
